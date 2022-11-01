9a9eec5e88fe869354f4dca979d045ab8a964079
mccaidan <mccaidan@umich.edu>
2022-10-31 21:18:46 -0400

    app created

---
+++ frontend/travelscrapbookapp/travelscrapbookapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travelscrapbookapp
+//
+//  Created by Aidan on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
