a7201689f3d13e617d050f02dd53622236ca12b8
mccaidan <mccaidan@umich.edu>
2022-11-10 12:29:05 -0500

    Made trip view work, added date picker and image picker

--- frontend/travelscrapbookapp/AddTripVC.swift
+++ frontend/travelscrapbookapp/AddTripVC.swift
@@ -16,9 +16,43 @@ import UIKit
 
 final class AddTripVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var pickStartDate: UITextField!
+    @IBOutlet weak var pickEndDate: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let dateStartPicker = UIDatePicker()
+        dateStartPicker.datePickerMode = .date
+        dateStartPicker.addTarget(self, action: #selector(dateStartChange(dateStartPicker:)), for: UIControl.Event.valueChanged)
+        dateStartPicker.preferredDatePickerStyle = .wheels
+        
+        pickStartDate.inputView = dateStartPicker
+        pickStartDate.text = formatDate(date: Date())  // Initialized with today's date
+        
+        let dateEndPicker = UIDatePicker()
+        dateEndPicker.datePickerMode = .date
+        dateEndPicker.addTarget(self, action: #selector(dateEndChange(dateEndPicker:)), for: UIControl.Event.valueChanged)
+        dateEndPicker.preferredDatePickerStyle = .wheels
+        
+        pickEndDate.inputView = dateEndPicker
+        pickEndDate.text = formatDate(date: Date())  // Initialized with today's date
+    }
+    
+    @objc func dateStartChange(dateStartPicker: UIDatePicker){
+        pickStartDate.text = formatDate(date: dateStartPicker.date)
+        
+    }
+    
+    @objc func dateEndChange(dateEndPicker: UIDatePicker){
+        pickEndDate.text = formatDate(date: dateEndPicker.date)
+        
+    }
+    
+    func formatDate(date: Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM dd yyyy"
+        return formatter.string(from: date)
     }
     
     weak var postImage: UIImageView!  // Probably chage this to whatever we want to store images in
